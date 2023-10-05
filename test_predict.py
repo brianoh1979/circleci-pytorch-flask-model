289b10bd7ec44c6b9f1dadd286684658121111ce
Vivek Kumar Maskara <maskaravivek@gmail.com>
2023-10-04 21:34:49 -0700

    With tests

---
+++ test_predict.py
@@ -0,0 +1,17 @@
+import unittest
+
+from predict import get_prediction
+from utils import get_bytes_from_image, download_image
+
+class TestDenseNetModel(unittest.TestCase):
+    def test_cat_image_inference(self):
+        
+        image_url = 'https://i.imgur.com/t5S6rkz.jpg'
+
+        download_image(image_url, '/tmp/image.jpg')
+        img_bytes = get_bytes_from_image('/tmp/image.jpg')
+
+        prediction = get_prediction(img_bytes)
+
+        self.assertEqual(prediction, ['n02124075', 'Egyptian_cat'])
+
